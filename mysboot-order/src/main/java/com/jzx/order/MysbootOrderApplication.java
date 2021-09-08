6db52ffb3f72d8be60c360517eb073ddb238550a
jiangzaixing <jiangzaixing@haitun.com>
2021-09-08 14:23:04 +0800

    order

---
+++ mysboot-order/src/main/java/com/jzx/order/MysbootOrderApplication.java
@@ -0,0 +1,16 @@
+package com.jzx.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description
+ * @Author Jiangzx
+ * @Date 2021/9/8
+ */
+@SpringBootApplication
+public class MysbootOrderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MysbootOrderApplication.class, args);
+    }
+}
